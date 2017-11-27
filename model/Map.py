5c0a5581401e64cbb78725bb3623222838d23c67
noocar <slavkory@gmail.com>
2017-11-27 23:34:20 +0300

    Strategy test updated.

--- model/Map.py
+++ model/Map.py
@@ -2,10 +2,10 @@ import networkx as nx
 
 
 class Map:
-    def __init__(self, responce):
+    def __init__(self, response):
         self.Graph = nx.Graph()
-        self.Graph.add_nodes_from([(point.pop('idx'),point) for point in responce["point"]])
-        self.Graph.add_edges_from([line.pop('point')+[line,] for line in responce["line"]])
+        self.Graph.add_nodes_from([(point.pop('idx'),point) for point in response["point"]])
+        self.Graph.add_edges_from([line.pop('point')+[line,] for line in response["line"]])
 
         self.pos = nx.spectral_layout(self.Graph, weight="length", scale=1, center=(0.5, 0.5))
 

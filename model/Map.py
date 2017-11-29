c19613104f7608b12958ca42ba1b3c8b43d49b56
noocar <slavkory@gmail.com>
2017-11-29 16:46:46 +0300

    Merged with master

--- model/Map.py
+++ model/Map.py
@@ -2,19 +2,19 @@ import networkx as nx
 
 
 class Map:
-    def __init__(self, responce):
+    def __init__(self, response):
         self.Graph = nx.Graph()
         self.Graph.add_nodes_from([(point.pop('idx'), point)
-                                   for point in responce["point"]])
+                                   for point in response["point"]])
         self.Graph.add_edges_from([
             line['point'] + [
                 {
                     'length': line['length']
                 },
-            ] for line in responce["line"]
+            ] for line in response["line"]
         ])
 
-        self.line = {line.pop('idx'): line for line in responce["line"]}
+        self.line = {line.pop('idx'): line for line in response["line"]}
 
         self.pos = nx.spring_layout(self.Graph, weight="length")
 

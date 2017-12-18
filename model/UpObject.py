7a23d98c014bfdd2ee50824b997419bdd5ae9e8d
Arthur <ar-2@tut.by>
2017-12-18 14:07:37 +0200

    add update to UpObject

--- model/UpObject.py
+++ model/UpObject.py
@@ -1,4 +1,8 @@
 class UpObject:
-    def __init__(self, post, train):
+    def __init__(self, post=[], train=[]):
+        self.post = post
+        self.train = train
+
+    def update(self, post, train):
         self.post = post
         self.train = train

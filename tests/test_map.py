6619c521b4dc2429d8bd524f7bf09c9d3044a963
Arthur <ar-2@tut.by>
2017-12-21 02:02:29 +0200

    add some tests

--- tests/test_map.py
+++ tests/test_map.py
@@ -1,167 +1,18 @@
 import pytest
-from model.Map import Map
-import json
-
-map_resp = json.loads('''{
-     "idx": 1,
-     "line": [
-         {
-             "idx": 1,
-             "length": 10,
-             "point": [
-                 1,
-                 7
-             ]
-         },
-         {
-             "idx": 2,
-             "length": 10,
-             "point": [
-                 8,
-                 2
-             ]
-         },
-         {
-             "idx": 3,
-             "length": 10,
-             "point": [
-                 9,
-                 3
-             ]
-         },
-         {
-             "idx": 4,
-             "length": 10,
-             "point": [
-                 10,
-                 4
-             ]
-         },
-         {
-             "idx": 5,
-             "length": 10,
-             "point": [
-                 11,
-                 5
-             ]
-         },
-         {
-             "idx": 6,
-             "length": 10,
-             "point": [
-                 12,
-                 6
-             ]
-         },
-         {
-             "idx": 7,
-             "length": 10,
-             "point": [
-                 7,
-                 8
-             ]
-         },
-         {
-             "idx": 8,
-             "length": 10,
-             "point": [
-                 8,
-                 9
-             ]
-         },
-         {
-             "idx": 9,
-             "length": 10,
-             "point": [
-                 9,
-                 10
-             ]
-         },
-         {
-             "idx": 10,
-             "length": 10,
-             "point": [
-                 10,
-                 11
-             ]
-         },
-         {
-             "idx": 11,
-             "length": 10,
-             "point": [
-                 11,
-                 12
-             ]
-         },
-         {
-             "idx": 12,
-             "length": 10,
-             "point": [
-                 12,
-                 7
-             ]
-         }
-     ],
-     "name": "map01",
-     "point": [
-         {
-             "idx": 1,
-             "post_id": 1
-         },
-         {
-             "idx": 2,
-             "post_id": null
-         },
-         {
-             "idx": 3,
-             "post_id": null
-         },
-         {
-             "idx": 4,
-             "post_id": null
-         },
-         {
-             "idx": 5,
-             "post_id": null
-         },
-         {
-             "idx": 6,
-             "post_id": null
-         },
-         {
-             "idx": 7,
-             "post_id": 2
-         },
-         {
-             "idx": 8,
-             "post_id": null
-         },
-         {
-             "idx": 9,
-             "post_id": null
-         },
-         {
-             "idx": 10,
-             "post_id": null
-         },
-         {
-             "idx": 11,
-             "post_id": null
-         },
-         {
-             "idx": 12,
-             "post_id": null
-         }
-     ]
- }''')
+from model.Map import Map, Position
+from util.WorldGetter import WorldGetter
 
 
-def test_init():
-    test_map = Map(map_resp)
+def test_correct_init():
+    factory = WorldGetter(3)
 
-    assert list(test_map.points.keys()) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    map_graph = Map(factory.get_map())
 
-    assert test_map.lines[3].length == 10
-    assert test_map.lines[3].start_point  == 9
-    assert test_map.lines[3].end_point  == 3
 
+def test_get_neighbors_pos():
+
+    factory = WorldGetter(3)
+    map_graph = Map(factory.get_map())
+
+    pos = Position(None, 2, 1)
+    assert(map_graph.get_neighbors_pos(pos) == [Position(2), Position(None, 2,2)])
--- tests/test_strategy.py
+++ tests/test_strategy.py
@@ -3,7 +3,7 @@ import pytest
 from Strategy_new import Strategy
 from util.WorldGetter import WorldGetter
 from model.Player import Player
-from model.Map import Map
+from model.Map import Map, Position
 from model.Objects import Objects
 
 
@@ -61,7 +61,9 @@ def test_get_target_points():
 
     # print([(market.product, market.point, map_graph.get_distance(1, market.point)) for market in strategy.objects.markets.values()])
 
-def test_move_to_point():
+
+def test_upgrade_targets():
+
     factory = WorldGetter(3)
 
     player = Player(factory.get_player())
@@ -70,7 +72,36 @@ def test_move_to_point():
 
     strategy = Strategy(player, map_graph, objects)
 
-    train = objects.trains[1]
+    assert(strategy.trains_points == {1: [], 2: []})
+    strategy.update_targets()
+    assert(strategy.trains_points == {1: [12, 1], 2: [32, 1]})
 
-    move = strategy._move_to_point(train, 11)
-    print(move)
+def test_get_invalid_pos():
+    factory = WorldGetter(3)
+
+    player = Player(factory.get_player())
+    map_graph = Map(factory.get_map())
+    objects = Objects(factory.get_objects())
+
+    strategy = Strategy(player, map_graph, objects)
+    
+
+    train = objects.trains[1]
+    invalid = [Position(None, 11, 2),
+               Position(None, 11, 4),
+               Position(None, 11, 3),
+               Position(32),
+               Position(56)]
+    assert(strategy.get_invalid_pos(train) == invalid)
+
+
+def test_find_path():
+    factory = WorldGetter(3)
+
+    player = Player(factory.get_player())
+    map_graph = Map(factory.get_map())
+    objects = Objects(factory.get_objects())
+
+    strategy = Strategy(player, map_graph, objects)
+
+    print(strategy.find_path(1, Position(33)))
